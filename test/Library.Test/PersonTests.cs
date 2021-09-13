637ed0c063d4d7a47b8fc83424fe5a686bf5ba60
Alejandra2431 <maria.sosadias@correo.ucu.edu.uy>
2021-09-12 22:22:49 -0300

    Tests

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 
 using UnitTestAndDebug;
 
-namespace Tests
+namespace PersonTests
 {
     public class Tests
     {
@@ -10,12 +10,50 @@ namespace Tests
         public void Setup()
         {
             // Insertá tu código de inicialización aquí
+            Person persona = new Person();
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void nombreCorrectoTest()
         {
-            // Insertá tu código  de pruebaaquí
+            string nombreCorrecto= "Alejandra"; 
+            persona = Person("Alejandra","");
+            Assert.AreEqual(nombreCorrecto,persona.Name);           
+        }
+
+        [Test]
+        public void cICorrectaTest()
+        {
+            string cICorrecta= "5.028.441-8"; 
+            persona = Person("","5028441-8");
+            Assert.AreEqual(cICorrecta,prueba.ID);           
+        }
+
+        [Test]
+        public void nombreIncorrectoTest()
+        {
+            string nombreIncorrecto= ""; 
+            persona = Person("","");
+            Assert.AreEqual(null,prueba.Name);           
+        }
+
+        [Test]
+        public void cIIncorrectaTest()
+        {
+            string cIIncorrecta= "123123123"; 
+            persona = Person("", cIIncorrecta);
+            Assert.AreEqual(null,prueba.ID);           
+        }
+
+        [Test]
+        public void IntroduceYourselfTest() 
+        {
+            string nombre = "Alejandra";
+            string ci = "50284418";
+            persona = Person("Alejandra", "5.028.441-8");
+            Assert.AreEqual(nombre, persona.Name);
+            Assert.AreEqual(ci, persona.Id);
+            Assert.AreEqual("Soy Alejandra y mi cédula es 5.028.441-8", persona.IntroduceYourself());
         }
     }
 }
